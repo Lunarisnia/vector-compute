e1d79dd9404e9140aacf475304a4f581a785feb8
Lounarisnia <r.arswendo.r@gmail.com>
2026-01-30 19:03:43 +0700

    wip: vk init

--- gpu-compute/src/engine/pandora/pandora.cpp
+++ gpu-compute/src/engine/pandora/pandora.cpp
@@ -1,5 +1,14 @@
 #include "engine/pandora/pandora.hpp"
+#include "VkBootstrap.h"
 
-void Pandora::Init() {}
+void Pandora::Init() {
+  vkb::InstanceBuilder builder;
+
+  vkb::Result<vkb::Instance> instance = builder.use_default_debug_messenger()
+                                            .request_validation_layers()
+                                            .set_headless()
+                                            .require_api_version(1, 3, 0)
+                                            .build();
+}
 
 void Pandora::Cleanup() { garbageCollector.Flush(); }
